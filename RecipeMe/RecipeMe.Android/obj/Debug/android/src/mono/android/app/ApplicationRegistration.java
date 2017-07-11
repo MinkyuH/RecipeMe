35abecc22955742627a8a31c5ccd682100ca7ed2
DESKTOP-TKFOKDI\kan02 <kan02100@naver.com>
2017-07-11 15:15:09 +1200

    Integrating Camera

--- RecipeMe/RecipeMe.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
+++ RecipeMe/RecipeMe.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -5,6 +5,7 @@ public class ApplicationRegistration {
 	public static void registerApplications ()
 	{
 				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("RecipeMe.Droid.MainApplication, RecipeMe.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md562fd48f6fa02f072d725d6d415fe88f9.MainApplication.class, md562fd48f6fa02f072d725d6d415fe88f9.MainApplication.__md_methods);
 		
 	}
 }
--- RecipeMe/RecipeMe/CustomVision.xaml.cs
+++ RecipeMe/RecipeMe/CustomVision.xaml.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +14,34 @@ namespace RecipeMe
         public CustomVision()
         {
             InitializeComponent();
+        }
+
+        private async void LoadCamera(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            MediaFile file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                PhotoSize = PhotoSize.Medium,
+                Directory = "Sample",
+                Name = $"{DateTime.UtcNow}.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                return file.GetStream();
+            });
+
+            file.Dispose();
         }
     }
 }
